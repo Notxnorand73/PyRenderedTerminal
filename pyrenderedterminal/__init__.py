0ac49704e3bc179e9b0f8ec23b85da9cde070b3c
Notxnorand73 <notxnor33415@outlook.com>
2026-03-19 17:38:12 -0500

    Update __init__.py

--- pyrenderedterminal/__init__.py
+++ pyrenderedterminal/__init__.py
@@ -1,3 +1,3 @@
-from .pyrenderedterminal import Scene, Actor, stamp, rect, Clock
+from .pyrenderedterminal import Scene, Actor, stamp, rect, Clock, collides, keybind
 
-__all__ = ["Scene", "Actor", "stamp", "rect", "Clock"]
+__all__ = ["Scene", "Actor", "stamp", "rect", "Clock", "keybind", "collides"]
