e794e79df01b8a2eb05a0bdf80c2a4078ba31d21
Notxnorand73 <notxnor33415@outlook.com>
2026-03-21 11:19:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="pyrenderedterminal",
@@ -6,16 +10,27 @@ setup(
     author="Notxnorand",
     author_email="notxnor33415@gmail.com",
     description="A simple terminal graphics library for learning programming",
-    long_description=open("README.md", encoding="utf-8").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Notxnorand73/PyRenderedTerminal",
+    project_urls={
+        "Documentation": "https://github.com/Notxnorand73/PyRenderedTerminal/blob/main/docs.html",
+        "Source": "https://github.com/Notxnorand73/PyRenderedTerminal",
+    },
     packages=find_packages(),
-    python_requires=">=3.7",
+    python_requires=">=3.9",
+    install_requires=[],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Education",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Games/Entertainment",
     ],
     keywords="terminal graphics python education learning",
 )
